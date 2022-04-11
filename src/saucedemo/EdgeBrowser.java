050787344347ca887b986019b6f1c5d0bc97eb60
jigna12 <kgotgb@gmail.com>
2022-04-11 02:50:34 +0100

    com-saucedemo

---
+++ src/saucedemo/EdgeBrowser.java
@@ -0,0 +1,36 @@
+package saucedemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
+public class EdgeBrowser {
+    public static void main(String[] args) {
+        String baseurl = "https://www.saucedemo.com/";
+        System.setProperty("webdriver.edge.driver","drivers/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+
+        driver.get(baseurl);//Launch the URL
+        driver.manage().window().maximize();//Maximize window
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));//We give Implicit time to driver
+
+        String title = driver.getTitle();//Get the title of the page
+        System.out.println("Page title = " +title);
+        System.out.println("Current URL = " +driver.getCurrentUrl());//Get Current URL
+
+        String p = driver.getPageSource();//Get the page-source
+        System.out.println("Page-Source=" +p);
+
+        WebElement usernameField = driver.findElement(By.id("user-name"));//Find the username field element
+        usernameField.sendKeys("abc123@gmail.com");
+
+        WebElement passwordField = driver.findElement(By.id("password"));//Find the password field element
+        passwordField.sendKeys("abc123");
+
+        driver.close();//Close the browser
+    }
+    }
+
---
+++ src/saucedemo/MultiBrowserTest.java
@@ -0,0 +1,92 @@
+package saucedemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class MultiBrowserTest {
+    public static void main(String[] args) {
+        String browser = "Firefox"; //"Chrome"//"Edge"
+        String baseurl = "https://www.saucedemo.com/";
+
+        if(browser.equalsIgnoreCase("Firefox"))
+        {
+            System.setProperty("webdriver.gecko.driver","drivers/geckodriver.exe");
+            WebDriver driver = new FirefoxDriver();
+
+            driver.get(baseurl);//Launch the URL
+            driver.manage().window().maximize();//Maximize window
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));//We give Implicit time to driver
+
+            String title = driver.getTitle();//Get the title of the page
+            System.out.println("Page title = " +title);
+            System.out.println("Current URL = " +driver.getCurrentUrl());//Get Current URL
+
+            String p = driver.getPageSource();//Get the page-source
+            System.out.println("Page-Source=" +p);
+
+            WebElement usernameField = driver.findElement(By.id("user-name"));//Find the username field element
+            usernameField.sendKeys("abc123@gmail.com");
+
+            WebElement passwordField = driver.findElement(By.id("password"));//Find the password field element
+            passwordField.sendKeys("abc123");
+
+            driver.close();//Close the browser
+
+        }else if(browser.equalsIgnoreCase("Edge"))
+        {
+            System.setProperty("webdriver.edge.driver","drivers/msedgedriver.exe");
+            WebDriver driver = new EdgeDriver();
+
+            driver.get(baseurl);//Launch the URL
+            driver.manage().window().maximize();//Maximize window
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));//We give Implicit time to driver
+
+            String title = driver.getTitle();//Get the title of the page
+            System.out.println("Page title = " +title);
+            System.out.println("Current URL = " +driver.getCurrentUrl());//Get Current URL
+
+            String p = driver.getPageSource();//Get the page-source
+            System.out.println("Page-Source=" +p);
+
+            WebElement usernameField = driver.findElement(By.id("user-name"));//Find the username field element
+            usernameField.sendKeys("abc123@gmail.com");
+
+            WebElement passwordField = driver.findElement(By.id("password"));//Find the password field element
+            passwordField.sendKeys("abc123");
+
+            driver.close();//Close the browser
+        }else if(browser.equalsIgnoreCase("Chrome"))
+        {
+            System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+
+            driver.get(baseurl);//Launch the URL
+            driver.manage().window().maximize();//Maximize window
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));//We give Implicit time to driver
+
+            String title = driver.getTitle();//Get the title of the page
+            System.out.println("Page title = " +title);
+            System.out.println("Current URL = " +driver.getCurrentUrl());//Get Current URL
+
+            String p = driver.getPageSource();//Get the page-source
+            System.out.println("Page-Source=" +p);
+
+            WebElement usernameField = driver.findElement(By.id("user-name"));//Find the username field element
+            usernameField.sendKeys("abc123@gmail.com");
+
+            WebElement passwordField = driver.findElement(By.id("password"));//Find the password field element
+            passwordField.sendKeys("abc123");
+
+            driver.close();//Close the browser
+        }else
+        {
+            System.out.println("Driver does not match");
+        }
+    }
+}
